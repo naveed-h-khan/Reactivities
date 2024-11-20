bc5c08d9195888b808da29a32d775201a36a63ff
Naveed Khan <naveed@logicsoft.co.in>
2024-11-20 18:40:53 +0530

    End of section 4

---
+++ Application/Activities/Delete.cs
@@ -0,0 +1,34 @@
+using MediatR;
+using Persistence;
+using static Application.Activities.Delete;
+
+
+namespace Application.Activities
+{
+    public class Delete
+    {
+        public class Command : IRequest
+        {
+            public Guid Id { get; set; }
+        }
+    }
+
+    public class Handler : IRequestHandler<Command>
+    {
+        private readonly DataContext _context;
+
+        public Handler(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Handle(Command request, CancellationToken cancellationToken)
+        {
+            var activity = await _context.Activities.FindAsync(request.Id);
+
+            _context.Activities.Remove(activity);
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
