cbc80803caca584aac6c67f0bef3a00d3a61c476
Naveed Khan <naveed@logicsoft.co.in>
2025-01-01 18:50:15 +0530

    End of section 6

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,15 +1,24 @@
 import { Button, Item, Label, Segment } from "semantic-ui-react";
 import { Activity } from "../../../app/models/interfaces/activity";
+import { SyntheticEvent, useState } from "react";
 
 export interface Props
 {
     activities: Activity[];
     selectActivity: (id:string) => void;
     deleteActivity:(id:string) => void;
+    submitting: boolean;
 }
 
-export default function({activities, selectActivity, deleteActivity}: Props)
+export default function({activities, selectActivity, deleteActivity, submitting}: Props)
 {
+    const [target, setTarget] = useState('');
+
+    function HandleActivityDelete(e: SyntheticEvent<HTMLButtonElement>, id: string){
+        setTarget(e.currentTarget.name);
+        deleteActivity(id);
+    }
+
     return(
         <Segment>
             <Item.Group divided>
@@ -24,7 +33,13 @@ export default function({activities, selectActivity, deleteActivity}: Props)
                             </Item.Description>
                             <Item.Extra>
                                 <Button onClick={() => selectActivity(activity.id)} floated='right' content='view' color='blue' />  
-                                <Button onClick={() => deleteActivity(activity.id)} floated='right' content='delete' color='red' />  
+                                <Button 
+                                    name = {activity.id}
+                                    loading={submitting && target === activity.id} 
+                                    onClick={(e) => HandleActivityDelete(e,activity.id)} 
+                                    floated='right' 
+                                    content='delete' 
+                                    color='red' />  
                                 <Label basic content={activity.category}></Label>
                             </Item.Extra>
                         </Item.Content>
